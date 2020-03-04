fcf13e90ca22dd6e2a4100b846345770816ba413
masaru-suzuki <masaru.suzuki.yamagata@gmail.com>
2020-03-04 22:03:33 +0900

    use flatlist

--- App.js
+++ App.js
@@ -1,45 +1,55 @@
-import React, { Component } from 'react';
-import { ScrollView, StyleSheet, View } from 'react-native';
+import React from 'react';
+import { SafeAreaView, View, FlatList, StyleSheet, Text } from 'react-native';
+import Constants from 'expo-constants';
 
-export default class App extends Component {
-  render() {
-    return (
-      <ScrollView style={styles.container}>
-        <View style={styles.boxLarge}/>
-        <ScrollView horizontal>
-          <View style={styles.boxSmall}/>
-          <View style={styles.boxSmall}/>
-          <View style={styles.boxSmall}/>
-          <View style={styles.boxSmall}/>
-          <View style={styles.boxSmall}/>
-          <View style={styles.boxSmall}/>
-        </ScrollView>
-        <View style={styles.boxLarge}/>
-        <View style={styles.boxSmall}/>
-        <View style={styles.boxLarge}/>
+const DATA = [
+  {
+    id: 0,
+    title: 'First Item',
+  },
+  {
+    id: 1,
+    title: 'Second Item',
+  },
+  {
+    id: 2,
+    title: 'Third Item',
+  },
+];
 
-      </ScrollView>
-    );
-  }
+function Item({ title }) {
+  return (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={({ item }) => <Item title={item.title} />}
+        keyExtractor={item => item.id}
+        flashScrollIndicators
+      />
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-   flex : 1
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
   },
-  boxLarge: {
-    width: 300,
-    height: 300,
-    marginBottom: 10,
-    marginRight: 10,
-    backgroundColor: 'steelblue',
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
   },
-  boxSmall: {
-    width: 100,
-    height: 100,
-    borderRadius: 50,
-    marginBottom: 10,
-    marginRight: 10,
-    backgroundColor: 'skyblue',
-  }
+  title: {
+    fontSize: 32,
+  },
 });
