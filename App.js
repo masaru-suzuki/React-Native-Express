51b2142f64078589110168f468ef9d048da13a09
masaru-suzuki <masaru.suzuki.yamagata@gmail.com>
2020-03-04 20:55:20 +0900

    create counter

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+class Counter extends React.Component{
+  state = {count: 0}
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        count:this.state.count + 1
+      })
+    }, 1000);
+  }
+  render(){
+    const {count} = this.state
+    const {color, size} = this.props
+    return(
+      <Text style={{color, fontSize: size}}>
+        {count}
+      </Text>
+    )
+  }
+}
+export default class App extends React.Component{
+  render(){
+    return (
+      <View style={styles.container}>
+        <Counter color={'pink'} size={12}/>
+        <Counter color={'red'} size={16}/>
+        <Counter color={'blue'} size={33}/>
+        <Counter color={'gray'} size={122}/>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
