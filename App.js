9b65a9177c4201faca5bf6e348b4c8bc71f8804d
masaru-suzuki <masaru.suzuki.yamagata@gmail.com>
2020-03-05 21:14:57 +0900

    use gesture

--- App.js
+++ App.js
@@ -1,43 +1,106 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { connect } from 'react-redux';
+import { StyleSheet, View, Text, PanResponder } from 'react-native';
 
-import { actionCreators } from './todoListRedux';
-import List from './List';
-import Input from './Input';
-import Title from './Title';
+export default class App extends Component {
+  constructor(props) {
+    super(props);
 
-const mapStateToProps = state => ({
-  todos: state.todos
-});
+    this.state = {
+      dragging: false,
+      initialTop: 200,
+      initialLeft: 300,
+      offsetTop: 0,
+      offsetLeft: 0
+    };
 
-class App extends Component {
-  onAddTodo = text => {
-    const { dispatch } = this.props;
-
-    dispatch(actionCreators.add(text));
-  };
-
-  onRemoveTodo = index => {
-    const { dispatch } = this.props;
-
-    dispatch(actionCreators.remove(index));
-  };
+    this.panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: this.handleStartShouldSetPanResponder,
+      onPanResponderGrant: this.handlePanResponderGrant,
+      onPanResponderMove: this.handlePanResponderMove,
+      onPanResponderRelease: this.handlePanResponderEnd,
+      onPanResponderTerminate: this.handlePanResponderEnd
+    });
+  }
 
   render() {
-    const { todos } = this.props;
+    const {
+      dragging,
+      initialTop,
+      initialLeft,
+      offsetTop,
+      offsetLeft
+    } = this.state;
+
+    // Update style with the state of the drag thus far
+    const style = {
+      backgroundColor: dragging ? 'skyblue' : 'steelblue',
+      top: initialTop + offsetTop,
+      left: initialLeft + offsetLeft
+    };
 
     return (
-      <View>
-        <Title>To-Do List</Title>
-        <Input
-          placeholder={'Type a todo, then hit enter!'}
-          onSubmitEditing={this.onAddTodo}
-        />
-        <List list={todos} onPressItem={this.onRemoveTodo} />
+      <View style={styles.container}>
+        <View
+          // Put all panHandlers into the View's props
+          {...this.panResponder.panHandlers}
+          style={[styles.square, style]}
+        >
+          <Text style={styles.text}>DRAG ME</Text>
+        </View>
       </View>
     );
   }
+
+  // Should we become active when the user presses down on the square?
+  handleStartShouldSetPanResponder = (e, gestureState) => {
+    return true;
+  };
+
+  // We were granted responder status! Let's update the UI
+  handlePanResponderGrant = (e, gestureState) => {
+    this.setState({ dragging: true });
+  };
+
+  // Every time the touch/mouse moves
+  handlePanResponderMove = (e, gestureState) => {
+    // Keep track of how far we've moved in total (dx and dy)
+    this.setState({
+      offsetTop: gestureState.dy,
+      offsetLeft: gestureState.dx
+    });
+  };
+
+  // When the touch/mouse is lifted
+  handlePanResponderEnd = (e, gestureState) => {
+    const { initialTop, initialLeft } = this.state;
+
+    // The drag is finished. Set the initialTop and initialLeft so that
+    // the new position sticks. Reset offsetTop and offsetLeft for the next drag.
+    this.setState({
+      dragging: false,
+      initialTop: initialTop + gestureState.dy,
+      initialLeft: initialLeft + gestureState.dx,
+      offsetTop: 0,
+      offsetLeft: 0
+    });
+  };
 }
 
-export default connect(mapStateToProps)(App);
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  square: {
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    width: 80,
+    height: 80,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    color: 'white',
+    fontSize: 12
+  }
+});
